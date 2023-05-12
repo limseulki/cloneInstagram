cb5e41944b8aa779199f1aee89f69e0650ce4c4b
DongKyu Lee <dldlehdrbrb@gmail.com>
2023-05-12 20:32:17 +0900

    Feat: ExceptionHandler 기능 추가 & 공통으로 쓰이는 응답 메세지 Dto 추가
    body : 슬기님 코드를 통해 가져온 ExceptionHandler 기능 코드 구현과 모든 서비스에 공통으로 쓰이는 응답 메세지 Dto 구현

---
+++ src/main/java/com/example/cloneinstagram/commonDto/ResponseMsgDto.java
@@ -0,0 +1,23 @@
+package com.example.cloneinstagram.commonDto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseMsgDto<T> {
+
+    private int status;
+    private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    public static <T> ResponseMsgDto<T> setSuccess(int status, String message, T data){
+        return new ResponseMsgDto<>(status, message, data);
+    }
+
+    public static <T> ResponseMsgDto<T> setFail(int status, String message){
+        return new ResponseMsgDto<>(status, message, null);
+    }
+}
---
+++ src/main/java/com/example/cloneinstagram/exception/CustomException.java
@@ -0,0 +1,10 @@
+package com.example.cloneinstagram.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
