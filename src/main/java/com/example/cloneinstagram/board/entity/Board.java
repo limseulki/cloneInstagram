42dfea44e5dab6aebea0fff58850d6d7556ee89a
limseulki <limslki333@hanmail.net>
2023-05-13 03:23:48 +0900

    Feat: Board 기능 구현
    게시글 작성, 수정, 삭제 기능 구현

--- src/main/java/com/example/cloneinstagram/board/entity/Board.java
+++ src/main/java/com/example/cloneinstagram/board/entity/Board.java
@@ -1,2 +1,49 @@
-package com.example.cloneinstagram.board.entity;public class Board {
+package com.example.cloneinstagram.board.entity;
+
+import com.example.cloneinstagram.board.dto.BoardRequestDto;
+import com.example.cloneinstagram.member.entity.Member;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Board extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String imageUrl;
+
+    @Column
+    private String contents;
+
+    @Column
+    private String nickName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    @JsonBackReference
+    private Member member;
+
+    @Builder
+    public Board(String imageUrl, String contents, Member member) {
+        this.imageUrl = imageUrl;
+        this.contents = contents;
+        this.member = member;
+        this.nickName = member.getNickName();
+    }
+
+    public void update(BoardRequestDto boardRequestDto) {
+        this.contents = boardRequestDto.getContents();
+    }
 }
--- src/main/java/com/example/cloneinstagram/love/entity/BoardLove.java
+++ src/main/java/com/example/cloneinstagram/love/entity/BoardLove.java
@@ -16,11 +16,11 @@ public class BoardLove {
     @Column(name = "boardlove_id")
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "board_id")
-    private Board board;
-
-    @ManyToOne
-    @JoinColumn(name = "member_id")
-    private Member member;
+//    @ManyToOne
+//    @JoinColumn(name = "board_id")
+//    private Board board;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "member_id")
+//    private Member member;
 }
