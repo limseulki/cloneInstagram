d0c28cdc228efb5321cec6424ce6c767117334e3
Joyanggi <jyg8033@gmail.com>
2023-05-13 10:39:24 +0900

    Feat: SwaggerFuncAdd
    1. BoardLoveController, CommentLoveController Swagger 관련 Annotation 추가(@Operation, @ApiResponses)

--- src/main/java/com/example/cloneinstagram/love/controller/BoardLoveController.java
+++ src/main/java/com/example/cloneinstagram/love/controller/BoardLoveController.java
@@ -3,6 +3,9 @@ package com.example.cloneinstagram.love.controller;
 import com.example.cloneinstagram.commonDto.ResponseMsgDto;
 import com.example.cloneinstagram.love.service.BoardLoveService;
 import com.example.cloneinstagram.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,8 @@ public class BoardLoveController {
     private final BoardLoveService boardLoveService;
 
     //피드 좋아요
+    @Operation(summary = "피드 좋아요 API", description = "피드 좋아요")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "피드 좋아요 성공")})
     @PostMapping("/boards/{boardId}")
     public ResponseMsgDto boardLove(@PathVariable(name = "boardId") Long id, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return boardLoveService.boardLove(id, userDetails.getUser());
--- src/main/java/com/example/cloneinstagram/love/controller/CommentLoveController.java
+++ src/main/java/com/example/cloneinstagram/love/controller/CommentLoveController.java
@@ -3,6 +3,9 @@ package com.example.cloneinstagram.love.controller;
 import com.example.cloneinstagram.commonDto.ResponseMsgDto;
 import com.example.cloneinstagram.love.service.CommentLoveService;
 import com.example.cloneinstagram.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,8 @@ public class CommentLoveController {
     private final CommentLoveService commentLoveService;
 
     //댓글 좋아요
+    @Operation(summary = "댓글 좋아요 API", description = "댓 좋아요")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "댓글 좋아요 성공")})
     @PostMapping("/comments/{commentId}")
     public ResponseMsgDto commentLove(@PathVariable(name = "commentId") Long id, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return commentLoveService.commentLove(id, userDetails.getUser());
