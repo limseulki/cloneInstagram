4c199c35fbae027e2d4e275ce71973a169e688d8
DongKyu Lee <dldlehdrbrb@gmail.com>
2023-05-15 16:20:54 +0900

    Fix : 회원가입 시 유효성 검사 기능 수정
    body : spring boot 2.3 version 이상부터는 spring-boot-starter-web 의존성 내부에 있던 validation이 사라짐 -> 따라서 validation 의존성을 따로 추가해야 함

--- src/main/java/com/example/cloneinstagram/member/dto/SignUpRequestDto.java
+++ src/main/java/com/example/cloneinstagram/member/dto/SignUpRequestDto.java
@@ -1,16 +1,18 @@
 package com.example.cloneinstagram.member.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
+
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class SignUpRequestDto {
-    @Pattern(regexp = "^[a-zA-Z0-9._]$", message = "유효한 닉네임을 입력해주세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9._]+$", message = "유효한 닉네임을 입력해주세요.")
     private String nickName;
 
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "유효한 이메일 주소를 입력해주세요.")
@@ -18,6 +20,6 @@ public class SignUpRequestDto {
     private String email;
 
     @Size(min = 8, max = 20, message = "비밀번호는 최소 8자에서 20자 사이로만 가능합니다.")
-    @Pattern(regexp = "^[a-zA-Z\\p{Punct}0-9]*$", message = "비밀번호는 대문자,소문자,숫자,특수문자만 가능합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).+$", message = "비밀번호는 대문자,소문자,숫자,특수문자만 가능합니다.")
     private String password;
 }
