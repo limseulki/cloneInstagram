6cc65206c033965c88a9934a75c283a33c61a989
DongKyu Lee <dldlehdrbrb@gmail.com>
2023-05-17 12:18:09 +0900

    Fix : 내가 작성한 게시물 댓글 출력 추가

--- src/main/java/com/example/cloneinstagram/board/dto/BoardResponseDto.java
+++ src/main/java/com/example/cloneinstagram/board/dto/BoardResponseDto.java
@@ -1,11 +1,15 @@
 package com.example.cloneinstagram.board.dto;
 
 import com.example.cloneinstagram.board.entity.Board;
+import com.example.cloneinstagram.comment.dto.CommentResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.persistence.OrderBy;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @AllArgsConstructor
@@ -14,12 +18,18 @@ public class BoardResponseDto {
     private String imageUrl;
     private String contents;
     private LocalDateTime createdAt;
+    @OrderBy("createdAt DESC")
+    private List<CommentResponseDto> commentResponseDtoList;
 
     @Builder
     public BoardResponseDto(Board board) {
         this.boardId = board.getId();
         this.imageUrl = board.getImageUrl();
         this.contents = board.getContents();
-        this.createdAt = board.getCreatedAt();
+        this.createdAt = LocalDateTime.parse(board.getCreatedAt());
+        this.commentResponseDtoList = board.getCommentList()
+                .stream()
+                .map(CommentResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
